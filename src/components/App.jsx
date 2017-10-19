b83edea2e293ee4231f326e91cf680baa1b7f511
Adam <adameliot1@gmail.com>
2017-10-18 18:18:57 -0700

    Finishing step 2 with first functional component and prop

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+        <div>
+            <h1>{props.heading}</h1>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-// ReactDOM.render(
-//     <This is some component />
-//     document.getElementById('root')
-// );
+ReactDOM.render(
+    <App heading="This is my heading as a prop" />,
+    document.getElementById('root')
+);
