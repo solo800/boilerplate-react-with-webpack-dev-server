4ca6eac4d79b8601149cc9209f3ee48e9f66782f
Adam <adameliot1@gmail.com>
2017-10-18 18:10:46 -0700

    Starting point for lunch and learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App.jsx';
 
-ReactDOM.render(
-    <App greeting='I am your React app, nice to meet you?' />,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <This is some component />
+//     document.getElementById('root')
+// );
